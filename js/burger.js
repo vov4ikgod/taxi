35192ecacc2f4d54f5a1c6c6123938a106f49411
Vova <86399820+vov4ikgod@users.noreply.github.com>
2022-03-26 11:30:50 +0500

    Add files via upload

--- js/burger.js
+++ js/burger.js
@@ -1,13 +1,18 @@
 const burger = document.querySelector('.burger');
 const popup = document.querySelector('.popup');
+const tel = document.querySelector('.tel-header');
 const menu = document.querySelector('.ul-menu-header');
+const titleLogo = document.querySelector('.logo-header');
 
 burger.addEventListener('click', () => {
     popup.classList.toggle('open');
     burger.classList.toggle('active-burger');
+    titleLogo.classList.toggle('active-title');
     renderPopup();
+    renderTel();
 });
 
 const renderPopup = () => {
     popup.appendChild(menu);
+    popup.appendChild(tel);
 }
