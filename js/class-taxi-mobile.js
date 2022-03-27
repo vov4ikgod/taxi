d756c9ec88ca817ac45d572efb489507288a730c
Vova <86399820+vov4ikgod@users.noreply.github.com>
2022-03-27 13:46:05 +0500

    Add files via upload

---
+++ js/class-taxi-mobile.js
@@ -0,0 +1,7 @@
+$(function() {
+    $('.slides-class-taxi-mobile').slick({
+        nextArrow: '<div class="class-taxi-next"><img src="./img/icon/next.svg" alt="next"></div>',
+        prevArrow: '<div class="class-taxi-prev"><img src="./img/icon/prev.svg" alt="prev"></div>',
+        dots: true
+    })
+}) 
