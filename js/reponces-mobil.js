4f623a11c55d1ecb9d609f5dfb0190633412a9d9
Vova <86399820+vov4ikgod@users.noreply.github.com>
2022-03-29 22:32:58 +0500

    Add files via upload

---
+++ js/reponces-mobil.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('.right-responces-mobil').slick({
+        infiniti: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        vertical: true,
+        nextArrow: '<div class="next-responses"><img src="./img/icon/next.svg" alt="next" style="display: flex;"></div>',
+        prevArrow: '<div class="prev-responses"><img src="./img/icon/prev.svg" alt="prev" style="display: flex;"></div>'
+    })
+}) 
